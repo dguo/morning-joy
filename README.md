8f33e1a6d5c058a45700679c2cacf52834526882
Danny Guo <dannyguo91@gmail.com>
2020-05-01 01:36:44 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Morning Joy
+
+Wake up to a happy text every day!
+
+## License
+
+[MIT](https://github.com/dguo/morning-joy/blob/master/LICENSE)
---
+++ index.js
@@ -0,0 +1,67 @@
+require("dotenv").config()
+const got = require("got");
+const twilio = require('twilio')(process.env.TWILIO_ACCOUNT_SID , process.env.TWILIO_AUTH_TOKEN);
+
+async function getUplingtingNewsPost() {
+    const response = await got("https://www.reddit.com/r/UpliftingNews/top.json?t=day").json();
+
+    if (!response.data.children[0].data) {
+        return null;
+    }
+
+    const {title, url} = response.data.children[0].data;
+    return {title, url};
+}
+
+async function getAwwPost() {
+    const response = await got("https://www.reddit.com/r/aww/top.json?t=day").json();
+
+    for (const post of response.data.children) {
+        if (post.data.post_hint !== "image") {
+            continue;
+        }
+
+        const {title, url} = post.data;
+        return {title, url};
+    }
+
+    return null;
+}
+
+
+async function sendText(body, mediaUrl) {
+    const text = await twilio.messages.create({
+         body,
+         mediaUrl: mediaUrl ? [mediaUrl] : [],
+         from: process.env.TWILIO_PHONE_NUMBER,
+         to: process.env.DESTINATION_PHONE_NUMBER
+    });
+
+    return text;
+}
+
+async function main() {
+   const upliftingNewsPost = await getUplingtingNewsPost();
+    if (upliftingNewsPost) {
+        const text = await sendText(`${upliftingNewsPost.title} ${upliftingNewsPost.url}`);
+        console.log(text);
+    }
+
+    const awwPost = await getAwwPost();
+    if (awwPost) {
+        const text = await sendText(awwPost.title, awwPost.url);
+        console.log(text);
+    }
+}
+
+// main().catch(error => console.error(error));
+
+exports.handler = async () => {
+    await main();
+
+    const response = {
+        statusCode: 200,
+    };
+
+    return response;
+};
